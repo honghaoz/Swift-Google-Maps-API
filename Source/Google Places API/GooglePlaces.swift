21a754dc61b908a816e3fb20d4735c2a1330a42d
Mamoun Diraneyya <mamouneyya@the-holy-macbook-pro.local>
2016-04-30 18:22:47 +0300

    Remove debugging stuff.

--- Source/Google Places API/GooglePlaces.swift
+++ Source/Google Places API/GooglePlaces.swift
@@ -67,7 +67,6 @@ public class GooglePlaces: GoogleMapsService {
             
             if response.result.isFailure {
                 NSLog("Error: GET failed")
-                print(response.result.error?.code)
                 completion?(response: nil, error: NSError(domain: "GooglePlacesError", code: -1, userInfo: nil))
                 return
             }
