1beca234bba1c7c8ee7de10e77c3fef48976f4cd
Honghao Zhang <zhh358@gmail.com>
2016-05-01 15:04:19 -0400

    Fixed compile bugs

--- GooglePlaces/GooglePlaces/GooglePlaces.h
+++ GooglePlaces/GooglePlaces/GooglePlaces.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Honghao Zhang. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for GooglePlaces.
 FOUNDATION_EXPORT double GooglePlacesVersionNumber;
