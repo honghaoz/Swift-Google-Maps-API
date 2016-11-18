6250c79efaa2cb4401538f3235b3ea1f3784c1a5
Afnan Ahmad <afnanahmaddd@gmail.com>
2016-11-18 18:36:06 +0500

    added .gitignore for example project

---
+++ Source/Google Places API/NearbyTypes.swift
@@ -0,0 +1,9 @@
+//
+//  NearbyTypes.swift
+//  GooglePlaces
+//
+//  Created by Afnan Ahmad on 18/11/2016.
+//  Copyright © 2016 Honghao Zhang. All rights reserved.
+//
+
+import Foundation
---
+++ Source/Google Places API/PlaceNearbyResponse.swift
@@ -0,0 +1,9 @@
+//
+//  PlaceNearbyResponse.swift
+//  GooglePlaces
+//
+//  Created by Afnan Ahmad on 18/11/2016.
+//  Copyright © 2016 Honghao Zhang. All rights reserved.
+//
+
+import Foundation
